5d321ba20a848f72fda8c845d33e398a2e647f9d
Pyrotronic <vlad.ivannikov.2003@gmail.com>
2023-05-10 01:48:17 +0300

    Initial commit

---
+++ ASD1/ASD1.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct info;
+
+typedef info* Pinfo;
+
+struct info
+{
+	int date[3] = { 0, 0, 0 };
+	string product;
+	string country;
+	int colvo;
+};
+
+void input(info&referense)
+{
+
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> referense.date[i];
+	}
+	cin >> referense.product;
+	cin >> referense.country;
+	cin >> referense.colvo;
+	cout << endl;
+}
+void print(const info& referense)
+{
+	for (int i = 0; i < 3; i++)
+	{
+
+		cout << referense.date[i] << " ";
+	}
+	cout << endl;
+	cout << referense.product << endl;
+	cout << referense.country << endl;
+	cout << referense.colvo << endl;
+	
+}
+
+int main()
+{
+	info tovar;
+	info& rtovar = tovar;
+	info* pinfo;
+	info mass[3];
+	pinfo = &mass[0];
+	for (int i = 0; i < 3; i++)
+	{
+		input(rtovar);
+		mass[i] = tovar;
+	}
+	int vsego = 0; 
+	string tovarbl;
+	cin >> tovarbl;
+	for (int i = 0; i < 3; i++)
+	{
+		if (mass[i].product == tovarbl)
+		{
+			cout << mass[i].country << endl;
+			vsego += mass[i].colvo;
+		}
+	}
+	cout << vsego;
+}
---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <iostream>
+#include<fstream>
+#include <string.h>
+#include<Windows.h>
+
+using namespace std;
+int main()
+{
+	typedef struct _tovar
+	{
+		char name[10];
+		char strana[10];
+		int kolvo;
+
+	} tovar;
+
+	FILE* f = fopen("tovar.txt", "r");
+
+	tovar  item[5];
+
+	if (f == NULL)
+	{
+		cout << "File not found";
+
+	}
+
+
+	char poisk[10];
+
+	cout << "Chto isem :";
+	cin >> poisk;
+
+
+
+	int templ = 0;
+
+	int i = -1;
+	while (!feof(f)) {
+		i++;
+		fscanf(f, "%s%d%s", item[i].name, &item[i].kolvo, item[i].strana);
+	}
+
+	for (int j = 0; j <= i; j++) {
+		if (item[j].name == poisk) {
+
+			printf("Srtana:\n%s postavlyaet %s v kolve %d shtuk.", item[i].name, item[i].strana, item[i].kolvo);
+		}
+	}
+	;
+	fclose(f);
+
+	getch();
+
+}
