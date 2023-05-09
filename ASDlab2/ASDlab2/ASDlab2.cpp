74b23b810d1d25d33b220596f17900e3e9a6291d
Pyrotronic <vlad.ivannikov.2003@gmail.com>
2023-05-10 01:51:37 +0300

    Лабы по Алгоритмам и системам данных

---
+++ ASDlab2/ASDlab2/ASDlab2.cpp
@@ -0,0 +1,211 @@
+﻿#include <iostream>
+#include<Windows.h>
+#include<stack>
+using namespace std;
+
+const int n = 100;
+struct Stack
+{
+    int A[n];
+    int count;
+};
+void Creation(Stack* p)
+{
+    p->count = 0;
+}
+int Full(Stack* p)
+{
+    if (p->count == 0) return 1;
+    else if (p->count == n) return -1;
+    else return 0;
+
+}
+void Add(Stack* p)
+{
+    int value;
+    cout << "Введите элемент > "; cin >> value;
+    p->A[p->count] = value;
+    p->count++;
+
+}
+
+void Add(Stack* p, int value)
+{
+    p->A[p->count] = value;
+    p->count++;
+
+}
+
+void Delete(Stack* p)
+{
+    p->count--;
+}
+int Top(Stack* p)
+{
+    return p->A[p->count - 1];
+}
+int Size(Stack* p)
+{
+    return p->count;
+}
+
+
+
+
+int main()
+{
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "1 - реализация стека с помощью пользовательской структуры.\n";
+    cout << "2 - реализация стека с помощью стандартной библиотеки.\n";
+    cout << "Введите способ реализации задачи:\n";
+    int num;
+    cin >> num;
+
+    switch (num)
+    {
+    case 1:
+    {
+        Stack main;
+        Creation(&main);
+
+        cout << "Введите количество элементов (меньше 100), которые хотите добавить в символьный стек:\n";
+        int size_stack;
+        cin >> size_stack;
+        cout << "Добавление элементов:\n";
+
+        for (int i = 0; i < size_stack; i++) Add(&main);
+
+        int* mass = new int[size_stack];
+        int max = Top(&main) - 1, index = size_stack - 1, maxindex = size_stack - 1;
+        while (!Full(&main))
+        {
+            if (Top(&main) > max)
+            {
+                max = Top(&main);
+                mass[index] = Top(&main);
+                Delete(&main);
+                maxindex = index;
+                index--;
+            }
+            else
+            {
+                mass[index] = Top(&main);
+                Delete(&main);
+                index--;
+            }
+        }
+
+        if (size_stack / 2 > maxindex)
+        {
+            for (int i = maxindex; i < size_stack / 2; i++)
+            {
+                mass[i] = mass[i + 1];
+            }
+            if (size_stack % 2 == 0)
+                mass[size_stack / 2 - 1] = max;
+            else
+                mass[size_stack / 2] = max;
+
+        }
+        else if (size_stack / 2 < maxindex)
+        {
+            for (int i = maxindex; i > size_stack / 2; i--)
+            {
+                mass[i] = mass[i - 1];
+            }
+            mass[size_stack / 2] = max;
+        }
+
+        for (int i = size_stack; i > 0; i--)
+        {
+            Add(&main, mass[i - 1]);
+        }
+
+        while (!Full(&main))
+        {
+            cout << Top(&main) << " ";
+            Delete(&main);
+        }
+
+        break;
+    }
+
+    case 2:
+    {
+        stack<int> main;
+        cout << "Введите количество элементов, которые хотите добавить в стек:\n";
+        int size_stack;
+        cin >> size_stack;
+        cout << "Добавление элементов:\n";
+
+        for (int i = 0; i < size_stack; i++)
+        {
+            int value;
+            cout << "Введите элемент > "; cin >> value;
+            main.push(value);
+        };
+
+        int* mass = new int[size_stack];
+        int max = main.top() - 1, index = size_stack - 1, maxindex = size_stack - 1;
+        while (!main.empty())
+        {
+            if (main.top() > max)
+            {
+                max = main.top();
+                mass[index] = main.top();
+                main.pop();
+                maxindex = index;
+                index--;
+            }
+            else
+            {
+                mass[index] = main.top();
+                main.pop();
+                index--;
+            }
+        }
+
+        if (size_stack / 2 > maxindex)
+        {
+            for (int i = maxindex; i < size_stack / 2; i++)
+            {
+                mass[i] = mass[i + 1];
+            }
+            if (size_stack % 2 == 0)
+                mass[size_stack / 2 - 1] = max;
+            else
+                mass[size_stack / 2] = max;
+
+        }
+        else if (size_stack / 2 < maxindex)
+        {
+            for (int i = maxindex; i > size_stack / 2; i--)
+            {
+                mass[i] = mass[i - 1];
+            }
+            mass[size_stack / 2] = max;
+        }
+
+        for (int i = size_stack; i > 0; i--)
+        {
+            main.push(mass[i - 1]);
+        }
+
+        while (!main.empty())
+        {
+            cout << main.top() << " ";
+            main.pop();
+        }
+
+        break;
+    }
+
+    default:
+        cout << "Ошибка!";
+        break;
+    }
+}
+
---
+++ ASDlab3/ASDlab3/ASDlab3.cpp
@@ -0,0 +1,140 @@
+﻿#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+const int N = 100;
+
+struct Queue
+{
+	int data[N];
+	int last;
+};
+
+void Creation(Queue* Q)
+{
+	Q->last = 0;
+}
+
+bool Full(Queue* Q)
+{
+	if (Q->last == 0) return true;
+	else return false;
+}
+
+void Add(Queue* Q)
+{
+	if (Q->last == N)
+	{
+		cout << "\nОчередь заполнена\n\n";
+		return;
+	}
+	int value;
+	cout << "\nЗначение: ";
+	cin >> value;
+	Q->data[Q->last++] = value;
+}
+
+void Add(Queue* Q, int value)
+{
+	if (Q->last == N)
+	{
+		cout << "\nОчередь заполнена\n\n";
+		return;
+	}
+
+	Q->data[Q->last++] = value;
+}
+
+void Delete(Queue* Q)
+{
+	for (int i = 0; i < Q->last && i < N; i++)
+		Q->data[i] = Q->data[i + 1];
+	Q->last--;
+}
+
+int Top(Queue* Q)
+{
+	return Q->data[0];
+}
+
+int Size(Queue* Q)
+{
+	return Q->last;
+}
+
+bool Comparison(Queue q1, Queue q2, int size)
+{
+	int count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (Top(&q1) == Top(&q2))
+		{
+			count++;
+			Delete(&q1);
+			Delete(&q2);
+		}
+	}
+
+	if (count == size) return 1;
+	else return 0;
+}
+
+void main()
+{
+	setlocale(0, "rus");
+
+	printf("Введите количество очередей, которые хотите ввести:\n");
+	int quantity_queue;
+	cin >> quantity_queue;
+
+	Queue* q = new Queue [quantity_queue];
+
+	for (int i = 0; i < quantity_queue; i++)
+	{
+		Creation(&q[i]);
+		printf("Введите количество элементов (не более 100), которые хотите ввести %d-ую в очередь:\n", i + 1);
+		int quantity_elem;
+		cin >> quantity_elem;
+
+		for (int j = 0; j < quantity_elem; j++)
+		{
+			printf("Введите %d-ый элемент в очередь: ", j + 1);
+			int elem;
+			cin >> elem;
+			Add(&q[i], elem);
+		}
+
+		cout << endl;
+	}
+
+	cout << "Нужные строки:\n";
+
+	for (int i = 1; i < quantity_queue; i++)
+	{
+		int count = 0;
+		for (int j = 0; j < quantity_queue; j++)
+		{
+			if (i == j) continue;
+
+
+			if (Size(&q[i]) != Size(&q[j])) continue;
+			else
+			{
+				if (Comparison(q[i], q[j], Size(&q[i])))
+					count++;
+			}
+		}
+
+		if (count == 1)
+		{
+			int size = Size(&q[i]);
+			for (int j = 0; j < size; j++)
+			{
+				cout << Top(&q[i]) << " ";
+				Delete(&q[i]);
+			}
+			cout << endl;
+		}
+	}
+
+}
